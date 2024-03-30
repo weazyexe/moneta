0482dbc5760cef7215e24500727cf54ebdff9f4d
Timur Zadvornov <weazy.exe@gmail.com>
2024-03-30 19:42:13 +0800

    initial commit

---
+++ app/src/main/java/dev/weazyexe/moneta/domain/Currency.kt
@@ -0,0 +1,8 @@
+package dev.weazyexe.moneta.domain
+
+data class Currency(
+    val symbol: String,
+    val name: String,
+    val code: String,
+    val emoji: String
+)
---
+++ app/src/main/java/dev/weazyexe/moneta/screens/home/HomeScreen.kt
@@ -0,0 +1,7 @@
+package dev.weazyexe.moneta.screens.home
+
+import com.slack.circuit.runtime.screen.Screen
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data object HomeScreen : Screen
